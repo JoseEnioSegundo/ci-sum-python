e62af2ef5a55c2c3284e1f29b0f7888d4e23eed9
JoseSegundo <jose.ardinoseg@gmail.com>
2026-06-08 09:14:32 -0300

    feat: atualiza mensagem de resposta da API para incluir versão de teste

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ def sum_values(a: int, b: int) -> int:
 
 @app.get("/")
 def read_root():
-    return {"message": "API de soma funcionando"}
+    return {"message": "API de soma funcionando teste 2.0"}
 
 
 @app.get("/sum/{a}/{b}")
